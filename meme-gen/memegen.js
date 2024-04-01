38b22b4c736d30b295be6b0c7f52a57a4ce1a93f
Anya Aven <leafeon96@gmail.com>
2024-04-01 18:19:16 -0400

    format and new FormData

--- meme-gen/memegen.js
+++ meme-gen/memegen.js
@@ -1,38 +1,49 @@
 "use strict";
 
-const imgList = document.querySelector("#img-list")
+const imgList = document.querySelector("#img-list");
 
-form.addEventListener("submit", addMeme)
+form.addEventListener("submit", addMeme);
 
-function addMeme(evt){
-  const form = evt.target;
+function addMeme(evt) {
   evt.preventDefault();
+  const form = evt.target;
+
   // Remove previos error messages
   const prevErrors = document.querySelectorAll(".error");
-  for(const error of prevErrors){
+  for (const error of prevErrors) {
     error.remove();
   }
 
   const memeDetails = {};
-  for(const input of form.children){
-    if(input.type !== "text") continue;
-
+  for (const [id, text] of new FormData(form)) {
     // If the user has not added a url, don't do anything
-    if(input.id === "img_url" && input.value === "") {
+    if (id === "img_url" && text === "") {
       createErrorMessage("Error, no URL entered");
       return;
     }
 
-    memeDetails[input.id] = input.value;
+    memeDetails[id] = text;
   }
+  // const memeDetails = {};
+  // for (const input of form.children) {
+  //   if (input.type !== "text") continue;
 
-  const meme = createMeme(memeDetails)
+  //   // If the user has not added a url, don't do anything
+  //   if (input.id === "img_url" && input.value === "") {
+  //     createErrorMessage("Error, no URL entered");
+  //     return;
+  //   }
+
+  //   memeDetails[input.id] = input.value;
+  // }
+
+  const meme = createMeme(memeDetails);
   imgList.appendChild(meme);
 
   form.reset();
 }
 
-function createMeme(details){
+function createMeme(details) {
   const memeContainer = document.createElement("div");
   memeContainer.classList.add("meme-container");
 
@@ -44,24 +55,24 @@ function createMeme(details){
   memeContainer.append(img);
 
   //Check if URL is a valid image
-  img.addEventListener("error", function() {
+  img.addEventListener("error", function () {
     createErrorMessage("URL is not a valid image");
     //TODO: find a better way stop executing the code IF the url is invalid.
     imgList.lastChild.remove();
-  })
+  });
 
   // Add optional title
   const title = makeDetail(details, "img_title");
   if (title !== null) memeContainer.append(title);
 
   //Add optional description
-  const description = makeDetail(details, "img_description")
+  const description = makeDetail(details, "img_description");
   if (description !== null) memeContainer.append(description);
 
   // Add remove button
   const removeButton = document.createElement("button");
   removeButton.classList.add("remove");
-  removeButton.innerText = "Delete"
+  removeButton.innerText = "Delete";
   memeContainer.append(removeButton);
   removeButton.addEventListener("click", removeMeme);
 
@@ -73,45 +84,45 @@ function createMeme(details){
   return memeContainer;
 }
 
-function makeDetail(details, id){
+function makeDetail(details, id) {
   const text = details[id];
-  if(text === "") return null;
+  if (text === "") return null;
 
   const div = document.createElement("div");
   div.innerText = text;
-  div.classList.add(id)
+  div.classList.add(id);
 
   return div;
 }
 
-function removeMeme(evt){
+function removeMeme(evt) {
   const target = evt.target;
 
   target.parentNode.remove();
 }
 
-function revealRemoveButton(evt){
-  const target = evt.target
+function revealRemoveButton(evt) {
+  const target = evt.target;
   const remove = target.lastChild;
 
-  remove.style.display = "block"
+  remove.style.display = "block";
 }
 
-function hideRemoveButton(evt){
-  const target = evt.target
+function hideRemoveButton(evt) {
+  const target = evt.target;
   const remove = target.lastChild;
 
-  remove.style.display = "none"
+  remove.style.display = "none";
 }
 
-function createErrorMessage(message){
+function createErrorMessage(message) {
   const container = document.querySelector("#form-container");
   const error = document.createElement("div");
   error.classList.add("error");
 
   error.innerText = message
-  ? message
-  : "Error";
+    ? message
+    : "Error";
 
   container.append(error);
 }
